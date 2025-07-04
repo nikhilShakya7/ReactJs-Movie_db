e1ccca6152292e5344f1267e383ac0510bfaee94
nikhilShakya7 <rsnikhil77@gmail.com>
2025-07-04 16:12:59 +0545

    `Added Yup validation schema and error handling to MovieForm component`

--- src/assets/MovieForm.jsx
+++ src/assets/MovieForm.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-
+import * as yup from "yup";
+import schema from "../services/validation";
 const MovieForm = ({ onSave, editingMovie }) => {
   const [movie, setMovie] = useState({
     name: "",
@@ -8,30 +9,46 @@ const MovieForm = ({ onSave, editingMovie }) => {
     rating: "",
     poster: "",
   });
+  const [errors, setErrors] = useState({});
   useEffect(() => {
     if (editingMovie) setMovie(editingMovie);
   }, [editingMovie]);
+
   const handleChange = (e) => {
     setMovie({ ...movie, [e.target.name]: e.target.value });
   };
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    onSave(movie);
-    if (!editingMovie) {
-      setMovie({ name: "", year: "", genere: "", rating: "", poster: "" });
+    try {
+      await schema.validate(movie, { abortEarly: false }); // throws if invalid
+      onSave(movie);
+      if (!editingMovie) {
+        setMovie({ name: "", year: "", genere: "", rating: "", poster: "" });
+      }
+      setErrors({});
+    } catch (err) {
+      if (err instanceof yup.ValidationError) {
+        const fieldErrors = err.inner.reduce((acc, cur) => {
+          acc[cur.path] = cur.message;
+          return acc;
+        }, {});
+        setErrors(fieldErrors);
+      }
     }
   };
   return (
     <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
-      <form onSubmit={handleSubmit} className="form">
+      <form onSubmit={handleSubmit} className="space-y-5">
         <input
-          className="w-full px-5 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 mb-5"
+          className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 mb-5"
           name="name"
           placeholder="Name"
           value={movie.name}
           onChange={handleChange}
-          required
         />
+        {errors.name && (
+          <p className="text-red-500 text-xs mt-1">{errors.name}</p>
+        )}
         <input
           name="year"
           placeholder="Year"
@@ -39,6 +56,9 @@ const MovieForm = ({ onSave, editingMovie }) => {
           onChange={handleChange}
           className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 mb-5"
         />
+        {errors.year && (
+          <p className="text-red-500 text-sm md-4">{errors.year}</p>
+        )}
         <input
           className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 mb-5"
           name="genere"
@@ -46,6 +66,9 @@ const MovieForm = ({ onSave, editingMovie }) => {
           value={movie.genere}
           onChange={handleChange}
         />
+        {errors.genere && (
+          <p className="text-red-500 text-sm mt-1">{errors.genere}</p>
+        )}
         <input
           name="rating"
           placeholder="Rating"
@@ -53,6 +76,10 @@ const MovieForm = ({ onSave, editingMovie }) => {
           onChange={handleChange}
           className="mb-5 w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
+        {errors.rating && (
+          <p className="text-red-500 text-sm ">{errors.rating}</p>
+        )}
+
         <input
           className="mb-5 w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           name="poster"
@@ -60,6 +87,9 @@ const MovieForm = ({ onSave, editingMovie }) => {
           value={movie.poster}
           onChange={handleChange}
         />
+        {errors.poster && (
+          <p className="text-red-500 text-sm">{errors.poster}</p>
+        )}
         <button
           type="submit"
           className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-200"
--- src/services/validation.js
+++ src/services/validation.js
@@ -1,18 +1,11 @@
-import * as yup from "yup";
-const schema=yup.object.shape({
+import * as yup from "yup"
+const currentYear = new Date().getFullYear();
+
+const schema= yup.object().shape({
     name:yup.string().required("Name is required"),
-    year:yup.number()
-    .typeError("Year must be a number")
-    .min(1878,"Thats earlier than the first motion picture!")
-    .max(new Date().getFullYear(),"Year cant be in future")
-    .required("Year is required"),
-    genere:yup.string().
-    required("Genre is required"),
-    rating:yup.number()
-    .typeError("Rating must be a number")
-    .max(100,"Maximum rating is 100")
-    .required("Rating is required"),
-    poster:yup.string()
-    .url("Invalid Link")
-    .required("Poster URL is required"),
-})
+    year:yup.number().typeError("Must be a number").min(1878,"Thats earlier than the first motion picture").max(new Date().getFullYear(),"You cannot be in the future").required("Date is required"),
+    genere:yup.string().required("Genre is required"),
+    rating:yup.number().typeError("Invalid rating").min(0,"Minimum Rating is 0").max(100,"Maximum rating is 100"),
+    poster:yup.string().url("Invalid URL").required("Poster URL is required"),
+});
+export default schema
